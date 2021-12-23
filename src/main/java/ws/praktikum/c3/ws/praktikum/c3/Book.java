b6f24198b8fce730082984649d022d06c6f23f96
DamasHimawan <damas261100@gmail.com>
2021-12-23 13:26:40 +0700

    first commit

---
+++ src/main/java/ws/praktikum/c3/ws/praktikum/c3/Book.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ws.praktikum.c3.ws.praktikum.c3;
+
+/**
+ *
+ * @author GF63-9SC
+ */
+public class Book {
+    public ArrayList<String> book = new ArrayList<>();
+    
+    public ArrayyList<String> newBook (){
+        book.add("Fisika 12");
+        book.add("Biologi 11");
+        book.add("Matematika 10");
+        return book;
+    }
+    
+}
---
+++ src/main/java/ws/praktikum/c3/ws/praktikum/c3/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package ws.praktikum.c3.ws.praktikum.c3.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
